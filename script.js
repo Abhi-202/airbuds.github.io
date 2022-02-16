416786f609b4ea2959b2391030c55a9513da6aeb
Abhi-202 <92498842+Abhi-202@users.noreply.github.com>
2022-02-16 18:42:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+navMenu = document.getElementById("nav-menu");
+
+navToggle.addEventListener("click", () => {
+	navMenu.classList.add("show-menu");
+});
+
+navClose.addEventListener("click", () => {
+	navMenu.classList.remove("show-menu");
+});
+
+
+// background
+
+function scrollHeader(){
+
+	const header = document.querySelector("header");
+	if(this.scrollY >= 50)  header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+
+	
+}
+
+window.addEventListener("scroll", scrollHeader );
